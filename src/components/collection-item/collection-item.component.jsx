6102f480b2a83761c7d29603900d6ce082b07a6b
vivek gupta <vivekgupta50455@gmail.com>
2021-01-12 11:44:58 +0530

    updated shop data with new data, updated cart utils to group items in collections

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import CollectionPreview from '../collection-preview/collection-preview.component';
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss'
+import {connect} from 'react-redux'
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, addItem}) => {
+    const {id, name, price, imageUrl} = item
+    return (
     <div className='collection-item' >
         <div 
             className='image'
@@ -18,6 +23,11 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
                 {price}
             </div>
         </div>
+        <CustomButton onClick={() => addItem(item)}
+        className = 'custom-button' inverted>Add to cart</CustomButton>
     </div>
-);
-export default CollectionItem
+)};
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+export default connect(null, mapDispatchToProps)(CollectionItem)
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,9 +8,9 @@ const CollectionPreview = ({title, items}) => {
         <div className='collection-preview' >
             <h1 className='title'>{title.toUpperCase() }</h1>
             <div className='preview'>
-                {items
+                {items 
                 .filter( (item, idx) => idx<4)
-                .map( ({id, ...otherItemProps}) => <CollectionItem key={id} {...otherItemProps} />
+                .map( (item) => <CollectionItem key={item.id} item={item} />
                 )}
             </div>
         </div>
