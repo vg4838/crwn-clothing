037897243ab2c13583f26a2775abcb3f4475313c
vivek gupta <vivekgupta50455@gmail.com>
2021-01-12 14:47:57 +0530

    updating our application with reselct , replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown.styles.scss'
 import {connect} from 'react-redux'
 import CartItem from '../cart-item/cart-item.component'
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
@@ -13,8 +14,8 @@ const CartDropdown = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = ({cart:{cartItems}}) => ({
-    cartItems
-})
+const mapStateToProps = (state) => {
+    return ({cartItems: selectCartItems(state)})
+}
 
 export default connect(mapStateToProps)(CartDropdown)
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -39,8 +39,9 @@ const Header = ({currentUser, hidden}) => (
         
     </div>
 )
-const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
+const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => {
+    return({
     currentUser,
-    hidden
-})
+    hidden})
+}
 export default connect(mapStateToProps)(Header)
