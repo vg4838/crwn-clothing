0238b67144e1f7210e18e2cfb472e48d6408e271
vivek gupta <vivekgupta50455@gmail.com>
2021-02-05 12:42:27 -0500

    changed server prod path

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
 
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
 }
 
