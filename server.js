8ca04a9b66e44cf404c46d9a40e63b32e3073dff
vivek gupta <vivekgupta50455@gmail.com>
2021-02-05 13:45:04 -0500

    changed server prod path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser');
+const path = require('path');
 
 if (process.env.NODE_ENV !== 'production')
     require('dotenv').config();
